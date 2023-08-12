b0a74c99933cc34086dac68aef52d155cb5295c1
skobayashi0417 <soma.kobayashi@stonybrookmedicine.edu>
2023-08-12 17:41:48 -0400

    rename files

---
+++ run_pipeline_Part2.py
@@ -0,0 +1,108 @@
+import os
+import json
+import sys
+sys.path.insert(0,'./0_ScaleWSIs')
+sys.path.insert(0,'./1_PatchExtraction')
+sys.path.insert(0,'./2_Inference')
+sys.path.insert(0,'./3_MeshMaps')
+sys.path.insert(0,'./4_registrationVisualization')
+from generateJSON import *
+from scaleWSIs_vipssave_secondLevel_ROI import *
+from extractmeshPatches_ROI import *
+from meshPredictions_ROI import *
+from meshPredictions_ROI_wLA_sepDest import *
+from generateMeshMaps_ROI import *
+from extractPatches_ROI import *
+from extractPatches_firstLevel_ROI import *
+from extractPatches_Moving_ROI import *
+from multiColor_overlayVisualization_ROI_newDetect_moreMarkers_sepDest_notbetrorgt_filterLA import *
+from extractOverlayVisPatches_ROI_newdetect_moreMarkers_sepDest import *
+
+def prepare_directories_ROI(config):
+    SCALED_ROI_DIR_SF8 = os.path.join(config['directories']['BASE_DIR'],'scaled_ROI_SF8')
+    if not os.path.exists(SCALED_ROI_DIR_SF8):
+        os.mkdir(SCALED_ROI_DIR_SF8)
+    
+    config['directories']['SCALED_ROI_DIR_SF8'] = SCALED_ROI_DIR_SF8
+    
+    config['directories']['DEST_DIR'] = config['directories']['BASE_DIR']
+    
+    if not os.path.exists(config['directories']['DEST_DIR']):
+        os.mkdir(config['directories']['DEST_DIR'])
+    
+    return config
+
+def runPipeline_part3(config):
+    config = prepare_directories_ROI(config)
+    print(config)
+
+    ### Scale HE and registered IHC to sf8
+    print('Scaling Registered Images to Second Level Scale (sf8)...')
+    #scale_WSI_secondLevel_ROI(ROI_DIR_POSTREG = config['directories']['ROI_postReg'],
+    #                          ROI_DIR_PREREG_REGISTERED = config['directories']['preRegROI_registrationOutputs'],
+    #                          SCALED_ROI_DIR = config['directories']['SCALED_ROI_DIR_SF8'],
+    #                          IHC_ORDER = config['SlideInfo']['IHC_Order'],
+    #                          PATCH_SIZE = config['PatchInfo']['PATCH_SIZE_SF8'], #use sf8 patch size as that is final expected scale
+    #                          WSI_EXTENSION = config['SlideInfo']['WSI_EXTENSION'],
+    #                          SCALE_FACTOR = 4) #going from sf2 to sf8
+    
+    ### Extract patches
+    print('Performing Mesh Patch Extraction on Second Level Scale (sf8)...')
+    #config = meshpatchExtraction_ROI(config = config)
+    
+    ### Run Mesh Predictions
+    print('Running Mesh Predictions...')
+    #config = perform_Meshpredictions_ROI(config = config)
+    
+    ### Run Mesh Predictions_wLA
+    print('Running Mesh Predictions with LA...')
+    #config = perform_Meshpredictions_ROI_wLA_sepDest(config = config)
+    
+    ### Generating Mesh Maps
+    print('Generating Mesh Maps...')
+    
+    config['directories']['meshPREDICTIONS_DIR_ROI_wLA'] = '/data01/shared/skobayashi/github_test/32pixelPatch_Predictions_ROI_wLA_sepDest'
+    config['directories']['meshPREDICTIONS_DIR_ROI'] = '/data01/shared/skobayashi/github_test/32pixelPatch_Predictions_ROI'
+    
+    #config = createMeshMaps_ROI(config)
+    
+    ### Extract Larger Patches
+    print('Performing Patch Extraction at Second Level Scale (sf8)...')
+    #config = patchExtraction_ROI(config)
+    
+    ### Extract Corresponding Patches at First Scale
+    print('Performing Extraction of Corresponding Patches at First Level Scale (sf2)...')
+    #config = patchExtraction_firstLevel_ROI(config)
+    
+    ### Extracting IHC Patches at Both Scales
+    print('Performing Corresponding Moving Patch Extraction at both scales...')
+    #config = patchExtraction_Moving_ROI(config)
+    
+    config['directories']['SCALED_ROI_DIR_SF8'] = '/data01/shared/skobayashi/github_test/scaled_ROI_SF8'
+    config['directories']['DEST_DIR'] = config['directories']['BASE_DIR']
+    config['directories']['bySampleMeshPatches_ROI']  = '/data01/shared/skobayashi/github_test/extracted_meshPatches_HE_ROI_SF8/bySample_Mesh'
+    config['directories']['meshPREDICTIONS_DIR_ROI_wLA'] = '/data01/shared/skobayashi/github_test/32pixelPatch_Predictions_ROI_wLA_sepDest'
+    config['directories']['meshPREDICTIONS_DIR_ROI'] = '/data01/shared/skobayashi/github_test/32pixelPatch_Predictions_ROI'
+    config['directories']['MESH_MAPS_ROI_DIR'] = '/data01/shared/skobayashi/github_test/meshMaps_ROI'
+    config['directories']['LA_meshMaps_dir']  = '/data01/shared/skobayashi/github_test/meshMaps_ROI_wLA'
+    config['directories']['bySamplePatches_ROI_FIXED_sf8'] = '/data01/shared/skobayashi/github_test/extractedPatches_ROI_FIXED_sf8/bySample'
+    config['directories']['bySamplePatches_ROI_FIXED_sf2'] = '/data01/shared/skobayashi/github_test/extractedPatches_ROI_FIXED_sf2/bySample'
+    
+    
+
+    ### Generate Single marker IHC detection masks... Generate IHC counts per patch as well
+    print('Generating vis Overlays...')
+    config = overlayVisualization_multiTarget_ROI_newDetect_moreMarkers_sepDest_notbetrorgt_filterLA(config)
+    
+    ### Extract patches from these overlay outputs
+    print('Extracting patches from vis Overlays...')
+    #extract_OverlayVisPatches_ROI_newDetect_moreMarkers(config)
+    
+    with open('config_part3.json','w') as outfile:
+        json.dump(config,outfile)
+
+    
+if __name__ == '__main__':
+    config = json.load(open('./config_part2.json'))
+    
+    runPipeline_part3(config)
---
+++ run_pipeline_Part3.py
@@ -0,0 +1,103 @@
+import os
+import json
+import sys
+sys.path.insert(0,'./1_PatchExtraction')
+sys.path.insert(0,'./2_Inference')
+sys.path.insert(0,'./5_ProbMaps')
+sys.path.insert(0,'./6_mouseModelInference')
+sys.path.insert(0,'./7_combineHE_IHC')
+from generateJSON import *
+from extractOverlapPatches_ROI import *
+from largerPatchPredictions_ROI import *
+from generateProbMaps_ROI import *
+from mergeMice_Involved_Uninvolved_ROI import *
+from RN_FeatureExtractor_ROI import *
+from conductPCA_ROI import *
+from kMeans_onPCA_ROI import *
+from merge_Uninvolved_InvolvedkMeansCounts_ROI import *
+from getProps_ROI import *
+from LDA_inference_ROI import *
+from sortkMeansClusters import *
+from combine_HE_IHC_Outputs_ROI import *
+from generate_HE_IHC_CSVs_andGraphs import *
+
+def prepare_directories(config):
+    INV_UNV_wIHC_BASE_DIR = os.path.join(config['directories']['BASE_DIR'],'INV_UNV_wIHC_Outputs_wUNINVkmeans_ROI')
+    if not os.path.exists(INV_UNV_wIHC_BASE_DIR):
+        os.mkdir(INV_UNV_wIHC_BASE_DIR)
+    
+    config['directories']['INV_UNV_wIHC_BASE_DIR_ROI'] = INV_UNV_wIHC_BASE_DIR
+    
+    return config
+
+def runPipeline(config):
+    ## extract HE overlap patches at sf8
+    print('Extracting overlapping patches...')
+    config = extract_overlapPatches_ROI(config)
+    
+    ## perform HE sf8 Involved versus Uninvolved predictions
+    print('Performing Involved versus Uninvolved predictions...')
+    config = perform_predictions_ROI(config)
+    
+    ## Generate HE SF8 ProbMaps
+    print('Generating Prob Maps...')
+    config = generateProbMaps_ROI(config)
+
+    ### Gather Involved and Uninvolved Patches...
+    print('Gathering Involved and Uninvolved Patches...')
+    config = mergeMice_ROI(config)
+
+    ### Perform RN Feature Extraction
+    print('Performing RN Feature Extraction on Involved Patches...')
+    config = RN_FeatureExtraction_ROI(config, 'Involved')
+    
+    print('Performing RN Feature Extraction on UNinvolved Patches...')
+    config = RN_FeatureExtraction_ROI(config, 'Uninvolved')
+    
+    ### Perform PCA
+    print('Conducting PCA on Involved Patches...')
+    config = performPCA_ROI(config, 'Involved')
+    
+    print('Conducting PCA on UNinvolved Patches...')
+    config = performPCA_ROI(config, 'Uninvolved')
+
+    ### Perform kMeans
+    print('performing kMeans on Involved Patches...')
+    config = kMeansPCA_ROI(config, 'Involved')
+    
+    print('performing kMeans on UNinvolved Patches...')
+    config = kMeansPCA_ROI(config, 'Uninvolved')
+
+    ### Merge InvolvedUninvolved Counts
+    print('Merging Involved and Uninvolved Counts...')
+    config = mergeCounts_ROI(config)
+
+    ### Generate Proportions
+    print('Generating Uninvolved and Involved k-mean Class Proportions...')
+    config = generateProps_ROI(config)
+        
+    ### Perform Mouse Model Inference
+    print('Performing LDA mouse model inference...')
+    config = LDA_infer_ROI(config)
+        
+    ### Sort kMeans Clusters to Visualize
+    print('Sorting kMeans Clusters...')
+    #sortClusters(config)
+    
+     ### Generate IHC HE agg DFs and graphs
+    print('Generating IHC HE agg dataframes and graph Outputs...')
+    IHC_HE_aggregatedDF_graphs_generation(config)
+    
+    ### Generate IHC HE Outputs
+    print('Generating visual IHC HE overlay Outputs...')
+    combine_HE_IHC_ROI(config)
+
+    
+if __name__ == '__main__':
+    generate_JSON()
+    
+    config = json.load(open('config_part3.json'))
+
+    config = prepare_directories(config)
+    
+    runPipeline(config)
